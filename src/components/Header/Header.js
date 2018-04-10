a239a8761981598af1c5fbddd2c5c8c29b2c0f67
Dani Carter <carte470@d.umn.edu>
2018-04-09 23:07:16 -0500

    jumbotron picture rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import "./Header.css";
 const Header = props => (
 	<div className="header">
 		<h1>Klicking Up With The Kardashians</h1>
-	<
 		<h2>Klick on a pic to earn points, but don't klick any more than once!</h2>
 	</div>
 	);
